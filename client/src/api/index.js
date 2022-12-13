a275b32670ef774c7fd04ee4cca6c1b8522e9101
Pranavi <76587467+Ms-Error@users.noreply.github.com>
2022-12-14 01:14:28 +0530

    Update index.js

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 
-const API = axios.create({ baseURL: "https://stack-over-flow.onrender.com" });
+const API = axios.create({ baseURL: "https://stack-over-floww.onrender.com" });
 API.interceptors.request.use((req) => {
   if (localStorage.getItem("Profile")) {
     req.headers.authorization = `Bearer ${
