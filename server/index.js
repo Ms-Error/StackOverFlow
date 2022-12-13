013be355280885efff0dc98968d3d74ea557a1dc
Pranavi <76587467+Ms-Error@users.noreply.github.com>
2022-12-14 01:15:06 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ dotenv.config()
 app.use(express.json({ limit: "30mb", extended: true }));
 app.use(express.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors({
-    origin:["http://localhost:3000","https://stack-over-flow.onrender.com"]
+    origin:["http://localhost:3000","https://stack-over-floww.onrender.com"]
   }
 ));
 
