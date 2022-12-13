36d327575a2104017330830fdaa2413dff916df8
Pranavi <76587467+Ms-Error@users.noreply.github.com>
2022-12-14 02:53:11 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -14,10 +14,7 @@ dotenv.config()
 
 app.use(express.json({ limit: "30mb", extended: true }));
 app.use(express.urlencoded({ limit: "30mb", extended: true }));
-app.use(cors({
-    origin:["http://localhost:3000","https://stack-over-floww.onrender.com"]
-  }
-));
+app.use(cors());
 
 app.get('/', (req, res) => {
   res.send("This is a StackOverflow clone api");
