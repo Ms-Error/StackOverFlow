53015e733906b5773f4a7e788846ab976c360115
Pranavi <tpranavi29@gmail.com>
2022-12-14 23:38:04 +0530

    changes

--- client/src/components/LeftBar/LeftBar.jsx
+++ client/src/components/LeftBar/LeftBar.jsx
@@ -2,28 +2,47 @@ import React from 'react'
 import '../LeftBar/LeftBar.css'
 import {NavLink} from 'react-router-dom'
 import Globe from '../../assets/Globe.png'
+
 const LeftBar = () => {
+  
   return (
-    <div className='left-sidebar'>
-   <nav className='side-nav'>
-       <NavLink to='/' className='side-nav-links' activeClassName='active'>
-           <p>Home</p>
-       </NavLink>
-       <div className='side-nav-div'>
-           <div><p>PUBLIC</p></div>
-           <NavLink to='/Questions' className='side-nav-links' activeClassName='active'>
-               <img src={Globe} height='20' alt="Globe" />
-               <p style={{paddingLeft: "10px"}}> Questions </p>
-           </NavLink>
-           <NavLink to='/Tags' className='side-nav-links' activeClassName='active' style={{paddingLeft: "40px"}}>
-               <p>Tags</p>
-           </NavLink>
-           <NavLink to='/User' className='side-nav-links' activeClassName='active' style={{paddingLeft: "40px"}}>
-               <p>Users</p>
-           </NavLink>
-       </div>
-   </nav>
-</div>
+    <div className="left-sidebar">
+      <nav className="side-nav">
+        <NavLink to="/" className="side-nav-links" activeClassName="active">
+          <p>Home</p>
+        </NavLink>
+        <div className="side-nav-div">
+          <div>
+            <p>PUBLIC</p>
+          </div>
+          <NavLink
+            to="/Questions"
+            className="side-nav-links"
+            activeClassName="active"
+          >
+            <img src={Globe} height="20" alt="Globe" />
+            <p style={{ paddingLeft: "10px" }}> Questions </p>
+          </NavLink>
+          <NavLink
+            to="/Tags"
+            className="side-nav-links"
+            activeClassName="active"
+            style={{ paddingLeft: "40px" }}
+          >
+            <p>Tags</p>
+          </NavLink>
+          <NavLink
+            to="/User"
+            className="side-nav-links"
+            activeClassName="active"
+            style={{ paddingLeft: "40px" }}
+          >
+            <p>Users</p>
+          </NavLink>
+         
+        </div>
+      </nav>
+    </div>
   );
 }
 
--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,6 +1,6 @@
 import React,{useEffect} from 'react'
 import {Link} from 'react-router-dom'
-import logo from '../../assets/logo-stackoverflow.png'
+import logo from '../../assets/logo.png'
 import { useSelector,useDispatch } from "react-redux";
 import './Navbar.css'
 import search from '../../assets/search.png'
@@ -35,43 +35,50 @@ const handleLogout = () => {
   return (
     <nav className="main-nav">
       <div className="navbar">
-        <Link to="/" className="nav-item nav-logo">
-          <img className="logo" src={logo} alt="logo" />
+        <Link to="/" className="nav-item nav-logo ">
+          <img src={logo} alt="logo" />
         </Link>
-        <Link to="/" className="nav-item nav-btn">
+        <Link to="/" className="nav-item nav-btn v-class-resp">
           About
         </Link>
-        <Link to="/" className="nav-item nav-btn">
+        <Link to="/" className="nav-item nav-btn v-class-resp">
           Products
         </Link>
-        <Link to="/" className="nav-item nav-btn">
+        <Link to="/" className="nav-item nav-btn v-class-resp">
           For teams
         </Link>
-        <form>
+        <form className="searchbar">
           <input type="text" placeholder="Search..."></input>
           <img src={search} alt="search" width={18} className="search-icon" />
         </form>
+
         {User === null ? (
           <Link to="/Auth" className="nav-item nav-links">
             Log in
           </Link>
         ) : (
           <>
-            <Avatar
-              backgroundColor="#009dff"
-              px="13px"
-              py="7px"
-              borderRadius="50%"
-              color="white"
-            >
-              <Link
-                to={`/User/${User?.result?._id}`}
-                style={{ color: "white", textDecoration: "none" }}
+            <div className="av">
+              <Avatar
+                backgroundColor="#009dff"
+                px="13px"
+                py="7px"
+                borderRadius="50%"
+                color="white"
               >
-                {User.result.name.charAt(0).toUpperCase()}
-              </Link>
-            </Avatar>
-            <button className="nav-item nav-links" onClick={handleLogout}>
+                <Link
+                  to={`/User/${User?.result?._id}`}
+                  style={{ color: "white", textDecoration: "none" }}
+                >
+                  {User.result.name.charAt(0).toUpperCase()}
+                </Link>
+              </Avatar>
+            </div>
+
+            <button
+              className="nav-item nav-links nav-logout"
+              onClick={handleLogout}
+            >
               Log out
             </button>
           </>
