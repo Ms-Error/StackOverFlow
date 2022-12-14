9d978bb9b4046f05db00b23a772dbab559f91d59
Pranavi <tpranavi29@gmail.com>
2022-12-14 11:57:26 +0530

    vercel config

--- server/index.js
+++ server/index.js
@@ -14,7 +14,9 @@ dotenv.config()
 
 app.use(express.json({ limit: "30mb", extended: true }));
 app.use(express.urlencoded({ limit: "30mb", extended: true }));
-app.use(cors());
+app.use(cors(
+ 
+));
 
 app.get('/', (req, res) => {
   res.send("This is a StackOverflow clone api");
